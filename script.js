f870b050551bb7469234be114f2d4ab8e4850de6
kitty <test1223570@mail.ru>
2020-04-02 16:40:29 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict'//подключаем стандарт ES??6
+
+/*var first = 1;//если вывести first до ее объявления, то в консоли будет undefined, то есть переменная уже существует, но не имеет значения
+
+let second = 2;//если же вывести second до объявления, консолька выдаст isn't defined,то есть такой переменной не существует вообще
+const third = 3;*/
+
+/*let arr = ['bruh', 'bruhbruh'];
+console.log(arr[0]);
+
+let answer = prompt('norm?', 'yeah');
+console.log(typeof(answer));*/
+
+//let answerNew = +prompt('norm?', 'yeah'); унарный плюс превращает строку в число (typeof(answerNew)=number)
+
+/*let a = "string " + 4;
+console.log(a);
+console.log(typeof(a));*/
+
+//let incr = 10, decr = 10;
+
+//console.log(incr++, decr--); сначала вывод incr, decr а потом их меняет
+//console.log(++incr, --decr); сначала меняет, а потом выводит их
+
+//console.log("2" == 2); true, тк идет сравнение по значению
+//=== - сравнение по типам данных
+
